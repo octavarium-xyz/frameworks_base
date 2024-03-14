d0305ec07c08dd3f155508bd8732e3b3ad797215
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-14 19:07:49 +0800

    EdgeEffect: Implement intensity-based edge scroll fling haptics
    Co-authored-by: Blaster4385 <venkateshchaturvedi12@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6393,6 +6393,12 @@ public final class Settings {
         public static final String GESTURE_NAVBAR_RADIUS = "gesture_navbar_radius";
 
         /**
+         * @hide
+         */
+        @Readable
+        public static final String EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
